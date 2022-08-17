689053cb3b377a1a6a3e7ba5409d4fe3205025e4
JavaDen44 <dem.denis44@gmail.com>
2022-08-17 17:09:22 +0300

    Added a field with cells. Added the functionality of clicking on a cell, at which a request will be sent to the server. Added to each cell the address of the servlet that is responsible for the logic. In the package “com.tictactoe“, we will create a class ”LogicServlet" and add a method to it that will get the index of the cell on which the click occurred. Created the ”InitServlet“ class, redefined the ”doGet" method in it, in which he created a new session, a playing field, put this playing field and a Sign list in the session attributes, and sent “forward" to the index.jsp page.

---
+++ src/main/java/com/tictactoe/LogicServlet.java
@@ -0,0 +1,28 @@
+package com.tictactoe;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "LogicServlet", value = "/logic")
+public class LogicServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        int index = getSelectedIndex(req);
+        resp.sendRedirect("/index.jsp");
+        super.doGet(req, resp);
+    }
+
+    /**
+     * Метод, который получает индекс ячейки, по которой произошел клик.
+     */
+    private int getSelectedIndex(HttpServletRequest request){
+        String click = request.getParameter("click");
+        boolean isNumeric = click.chars().allMatch(Character::isDigit);
+        return isNumeric ? Integer.parseInt(click) : 0;
+    }
+}
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -4,10 +4,27 @@
 <html>
 <head>
     <title>Tic-Tac-Toe</title>
+    <link href="static/main.css" rel="stylesheet">
 </head>
 <body>
 <h1>Tic-Tac-Toe</h1>
-
+<table>
+    <tr>
+        <td onclick="window.location='/logic?click=0'">0</td>
+        <td onclick="window.location='/logic?click=0'">1</td>
+        <td onclick="window.location='/logic?click=0'">2</td>
+    </tr>
+    <tr>
+        <td onclick="window.location='/logic?click=0'">3</td>
+        <td onclick="window.location='/logic?click=0'">4</td>
+        <td onclick="window.location='/logic?click=0'">5</td>
+    </tr>
+    <tr>
+        <td onclick="window.location='/logic?click=0'">6</td>
+        <td onclick="window.location='/logic?click=0'">7</td>
+        <td onclick="window.location='/logic?click=0'">8</td>
+    </tr>
+</table>
 
 <script>
 
