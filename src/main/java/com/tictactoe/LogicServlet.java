c9c7795a40c5deac7b33c79b92663b483465530e
malyniak <malyniakvolodiaaa@gmail.com>
2023-10-31 08:17:25 +0200

    create field in jsp, add start servlet and logic of putting Sign.cross in cell

---
+++ src/main/java/com/tictactoe/LogicServlet.java
@@ -0,0 +1,52 @@
+package com.tictactoe;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
+@WebServlet(name = "LogicServlet", value = "/logic")
+public class LogicServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        HttpSession currentSession = req.getSession();
+        Field field = extractField(currentSession);
+        int index = getSelectedIndex(req);
+        Sign currentSign = field.getField().get(index);
+        if (Sign.EMPTY != currentSign) {
+            RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/index.jsp");
+            dispatcher.forward(req, resp);
+            return;
+        }
+        field.getField().put(index, Sign.CROSS);
+        int emptyFieldIndex = field.getEmptyFieldIndex();
+        List<Sign> data = field.getFieldData();
+        currentSession.setAttribute("data", data);
+        currentSession.setAttribute("field", field);
+        resp.sendRedirect("/index.jsp");
+    }
+
+    private int getSelectedIndex(HttpServletRequest request) {
+        String click = request.getParameter("click");
+        boolean isNumeric = click.chars().allMatch(Character::isDigit);
+        return isNumeric ? Integer.parseInt(click) : 0;
+    }
+
+    private Field extractField(HttpSession currentSession) {
+        Object fieldAttribute = currentSession.getAttribute("field");
+        if (Field.class != fieldAttribute.getClass()) {
+            currentSession.invalidate();
+            throw new RuntimeException("Session is broken, try one more time");
+        }
+        return (Field) fieldAttribute;
+    }
+
+}
+
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,17 +1,31 @@
+<%@ page import="com.tictactoe.Sign" %>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 
 <!DOCTYPE html>
 <html>
 <head>
     <title>Tic-Tac-Toe</title>
+    <link href="static/main.css" rel="stylesheet">
+    <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+    <script src="<c:url value="/static/jquery-3.6.0.min.js"/>"></script>
 </head>
 <body>
 <h1>Tic-Tac-Toe</h1>
-
-
-<script>
-
-</script>
-
+<table>
+    <tr>
+        <td onclick="window.location='/logic?click=0'">${data.get(0).getSign()}</td>
+        <td onclick="window.location='/logic?click=1'">${data.get(1).getSign()}</td>
+        <td onclick="window.location='/logic?click=2'">${data.get(2).getSign()}</td>
+    </tr>
+    <tr>
+        <td onclick="window.location='/logic?click=3'">${data.get(3).getSign()}</td>
+        <td onclick="window.location='/logic?click=4'">${data.get(4).getSign()}</td>
+        <td onclick="window.location='/logic?click=5'">${data.get(5).getSign()}</td>
+    </tr>
+    <tr>
+        <td onclick="window.location='/logic?click=6'">${data.get(6).getSign()}</td>
+        <td onclick="window.location='/logic?click=7'">${data.get(7).getSign()}</td>
+        <td onclick="window.location='/logic?click=8'">${data.get(8).getSign()}</td>
+    </tr>
+</table>
 </body>
-</html>
